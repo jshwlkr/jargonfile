e7936a263bdc067a032d19a7ed72854e987e5c1e
Joshua Walker <joshw@phiregroup.com>
2014-11-19 14:14:28 -0500

    proposed project structure, gulp work

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    fileinclude = require('gulp-file-include'),
+    livereload = require('gulp-livereload');
+
+var paths = {
+  templates: './templates/',
+  sass: './sass/',
+  dist: './dist/'
+};
 
 gulp.task('default', function() {
 
