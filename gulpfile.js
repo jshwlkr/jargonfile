a8ef22a14419183787faad12aae6da28b2b37c42
Joshua Walker <joshw@phiregroup.com>
2014-11-21 14:52:17 -0500

    more initial gulp work, not ready for use yet

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,25 @@ var paths = {
   dist: './dist/'
 };
 
+gulp.task('html', function() {
+  return  gulp.src(path.join(paths.templates, '*.tpl.html'))
+    .pipe(fileinclude())
+    .pipe(rename({
+      extname: ""
+     }))
+    .pipe(rename({
+      extname: ".html"
+     }))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('sass', function() {
+  return gulp.src(path.join(paths.sass, '*.scss'))
+    .pipe(sass({ style: 'expanded', sourceComments: 'map', errLogToConsole: true}))
+    .pipe(autoprefixer('last 2 version', "> 1%", 'ie 8', 'ie 9'))
+    .pipe(gulp.dest(paths.dist + '/css'));
+});
+
 gulp.task('default', function() {
 
 });
